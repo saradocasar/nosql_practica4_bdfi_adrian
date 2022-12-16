87cef7861f453f3ec42489812c89693a1a53d7cb
acallejasz <5aadrianc@gmail.com>
2022-12-16 17:00:58 +0100

    Fixed but bugs yet

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/acallejasz/nosql_practica4_bdfi_adrian.git
-cd /nosql_practica4_bdfi
+cd /nosql_practica4_bdfi_adrian
 npm install
 npm run seed
 npm start
